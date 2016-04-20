b7453a3e5c865963e03391e017572b0df57d8a29
Lawrence Moore <lawrence2978@hotmail.com>
2016-04-20 15:52:46 -0400

    Showing ingredients

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,27 +1,33 @@
 'use strict';
 
 angular.module('baristaMaticApp')
-  .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
+    .controller('MainCtrl', function ($scope, $http, socket) {
+        $scope.awesomeThings = [],
+        $scope.ingredientList = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
+        $http.get('/api/things').success(function(awesomeThings) {
+            $scope.awesomeThings = awesomeThings;
+            socket.syncUpdates('thing', $scope.awesomeThings);
+        });
+
+        $http.get('/api/ingredients').success(function(ingredientList) {
+            $scope.ingredientList = ingredientList;
+            socket.syncUpdates('ingredient', $scope.ingredientList);
+        });
+
+        $scope.addThing = function() {
+            if($scope.newThing === '') {
+                return;
+            }
+            $http.post('/api/things', { name: $scope.newThing });
+            $scope.newThing = '';
+        };
+
+        $scope.deleteThing = function(thing) {
+            $http.delete('/api/things/' + thing._id);
+        };
+
+        $scope.$on('$destroy', function () {
+            socket.unsyncUpdates('thing');
+        });
     });
-
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
-
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
-    });
-  });
